27fe7a13754da3bac53c2eef0594e3d33477cad2
RANJEET KUMAR MEHTA <121413963+Ranjeetmehta@users.noreply.github.com>
2023-08-25 19:55:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+// script.js
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'Aerosmith', 'The Beatles', 'Pink Floyd'];
+
+function stripArticle(name) {
+    // Function to remove leading articles (a, an, the)
+    return name.replace(/^(a |an |the )/i, '');
+}
+
+bandNames.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
+
+const ulElement = document.getElementById('band');
+
+for (let i = 0; i < bandNames.length; i++) {
+    const liElement = document.createElement('li');
+    liElement.textContent = bandNames[i];
+    ulElement.appendChild(liElement);
+}
